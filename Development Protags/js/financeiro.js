313d9fb3c88f8c0aeec1ff4e12f32132f3cdcaaf
Tiago <thosouza@sga.pucminas.br>
2023-05-14 20:24:48 -0300

    14/05/2023

--- Development Protags/js/financeiro.js
+++ Development Protags/js/financeiro.js
@@ -1,18 +1,36 @@
 
 // Variáveis do formulário
 
-var entrada = 150 /*document.querySelector('#entrada')*/
-var saida = 200 /*document.querySelector('#saida')*/
-var resultInfo = entrada - saida
+var entrada = 250.40 /*document.querySelector('#entrada')*/
+var saida = 200.50 /*document.querySelector('#saida')*/
 
-function a(){
+
+var data = new Date();
+var dia = String(data.getDate()).padStart(2, '0');
+var mes = String(data.getMonth() + 1).padStart(2, '0');
+var ano = data.getFullYear();
+var FirstDay = "01" + '/' + mes + '/' + ano;
+var LastDay = "31" + '/' + mes + '/' + ano;
+
+
+function inicializarMain(){
+    
+    let resultInfo = parseInt(entrada) - parseInt(saida)
     if (resultInfo > 0)
-        resultInfo1.innerHTML = `Positivo ${resultInfo}`
+        resultInfo1.innerHTML = `Receitas maiores que despesas ${resultInfo}`,
+        resultInfo2.innerHTML = `Saldo disponivel: R$ ${resultInfo}`
     else {
-        resultInfo1.innerHTML = `Negativo ${resultInfo}`
-    }
+        resultInfo1.innerHTML = `Despesas maiores que receitas ${resultInfo}`
+        resultInfo2.innerHTML = `Saldo disponivel: (-) R$ ${resultInfo}`
     }
 
+    period_info_1.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
+    period_info_2.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
+
+    resultFiltro.innerHTML = `Movimentação entre: ${FirstDay} a ${LastDay}`
+    }
+document.onload = inicializarMain()
+    
 // Filtro data do Grafico
 
 
@@ -20,10 +38,11 @@ function a(){
 
 function Filtrar() {
 
-    let dataInicio = document.querySelector('input#dataInicio')
-    let dataFim = document.querySelector('input#dataFim')
-    let result = String(dataInicio.value) + " a " + String(dataFim.value)
-    resultFiltro.innerHTML = `Movimentação entre  ${result}.`
+    let dataInicio = document.querySelector('input#dataInicio').value;
+    let dataFim = document.querySelector('input#dataFim').value
+    let dataInicFim  =  dataInicio.replace(/(\d*)-(\d*)-(\d*).*/, '$3/$2/$1') 
+                   + " a " + dataFim.replace(/(\d*)-(\d*)-(\d*).*/, '$3/$2/$1');
+    resultFiltro.innerHTML = `Movimentação entre  ${dataInicFim}.`
 
 }
 
