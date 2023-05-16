00c26b5edd0a49ba04742006613bb04e83a1d797
Tiago <thosouza@sga.pucminas.br>
2023-05-16 12:24:20 -0300

    Alterações 16/05/2023 12:24

--- Development Protags/js/financeiro.js
+++ Development Protags/js/financeiro.js
@@ -1,8 +1,7 @@
-
 // Variáveis do formulário
 
 var entrada = 250.40 /*document.querySelector('#entrada')*/
-var saida = 200.50 /*document.querySelector('#saida')*/
+var saida = 220.50 /*document.querySelector('#saida')*/
 
 
 var data = new Date();
@@ -13,28 +12,39 @@ var FirstDay = "01" + '/' + mes + '/' + ano;
 var LastDay = "31" + '/' + mes + '/' + ano;
 
 
-function inicializarMain(){
+
+// Ao clicar em 'Sair', apaga o token de acesso, exigindo um novo login
+function logout() {
+    localStorage.removeItem('token')
+    window.location.href = './login.html'
+}
+// Não permite que o usuário entre no sistema sem estar logado
+if (localStorage.getItem('token') === null) {
+
+    alert('Você precisa estar logado para acessar essa página')
+    window.location.href = './login.html'
+
+} else {
+
+    window.onload = function(){
+        let resultInfo = parseInt(entrada) - parseInt(saida)
+        if (resultInfo > 0)
+            resultInfo1.innerHTML = `Receitas maiores que despesas`,
+            resultInfo2.innerHTML = `Saldo disponivel: R$ ${resultInfo}`,
+            resultInfo1.setAttribute('style', 'color: green'),
+            resultInfo2.setAttribute('style', 'color: green')
+        else {
+            resultInfo1.innerHTML = `Despesas maiores que receitas`,
+            resultInfo2.innerHTML = `Saldo disponivel: <b>(-)</b> R$ ${resultInfo}`,
+            resultInfo1.setAttribute('style', 'color: red')
+        }
     
-    let resultInfo = parseInt(entrada) - parseInt(saida)
-    if (resultInfo > 0)
-        resultInfo1.innerHTML = `Receitas maiores que despesas`,
-        resultInfo2.innerHTML = `Saldo disponivel: R$ ${resultInfo}`
-    else {
-        resultInfo1.innerHTML = `Despesas maiores que receitas`
-        resultInfo2.innerHTML = `Saldo disponivel: (-) R$ ${resultInfo}`
-    }
-
-    period_info_1.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
-    period_info_2.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
-
-    resultFiltro.innerHTML = `Movimentação entre: ${FirstDay} a ${LastDay}`
-    }
-document.onload = inicializarMain()
+        period_info_1.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
+        period_info_2.innerHTML = `Periodo: ${FirstDay} até ${LastDay}`
     
-// Filtro data do Grafico
-
-
-
+        resultFiltro.innerHTML = `Movimentação entre: ${FirstDay} a ${LastDay}`
+        }
+}
 
 function Filtrar() {
 
