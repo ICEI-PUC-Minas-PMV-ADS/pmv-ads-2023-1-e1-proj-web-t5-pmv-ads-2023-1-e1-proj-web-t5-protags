4b0f1ea23ef56a5953652c8a6baf789c87b998b5
Lucas Gabriel <lenis@sga.pucminas.br>
2023-05-19 10:03:17 -0300

    teste

--- Development Protags/js/desempenho.js
+++ Development Protags/js/desempenho.js
@@ -81,25 +81,10 @@ for (const conta of dbpcontas) {
 }
 
 // Utilize o array de valores no gráfico
-const ctx = document.getElementById('myChart');
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'],
-    datasets: [{
-      label: 'Valores',
-      data: valores,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+
+
+
+
 
 
 
