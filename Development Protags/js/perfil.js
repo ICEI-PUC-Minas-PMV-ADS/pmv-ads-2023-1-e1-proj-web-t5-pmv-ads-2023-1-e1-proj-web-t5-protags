22299ff067ddad0c147a258a214810b0faa866cc
alineazedias <aline.azedias@gmail.com>
2023-05-29 13:53:12 -0300

    perfil

--- Development Protags/js/perfil.js
+++ Development Protags/js/perfil.js
@@ -8,16 +8,15 @@
 //     password: "password123"
 // };
 
-
 const userPerfil = JSON.parse(localStorage.getItem('user'));
 
 
 document.getElementById('nomeEmpresa').textContent = userPerfil.novoNomeEmpresa;
 document.getElementById('cnpj').textContent = userPerfil.novoCNPJ;
 document.getElementById('adminPassword').textContent = userPerfil.senhaadmim;
-document.getElementById('name').textContent = userPerfil.name;
-document.getElementById('username').textContent = userPerfil.novoUser;
+document.getElementById('name').textContent = userPerfil.novoNomeUsuario;
+document.getElementById('username').textContent = userPerfil.novoUsuario;
 document.getElementById('email').textContent = userPerfil.novoEmail;
-document.getElementById('password').textContent = userPerfil.novaSenha;
+document.getElementById('password').textContent = userPerfil.alterarSenha;
 
 
