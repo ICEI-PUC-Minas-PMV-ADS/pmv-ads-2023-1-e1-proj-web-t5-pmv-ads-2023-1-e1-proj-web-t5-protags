deb008e7d1ee1df2c664ea5421b61f8773f6606b
alineazedias <aline.azedias@gmail.com>
2023-06-07 22:29:52 -0300

    perfil 7

--- Development Protags/js/perfil.js
+++ Development Protags/js/perfil.js
@@ -1,38 +1,37 @@
 
 const userPerfil = JSON.parse(localStorage.getItem('user'));
-
-// const nomeEmpresa = document.getElementById('nomeEmpresa');
-// const cnpj = document.getElementById('cnpj');
-// const adminPassword = document.getElementById('adminPassword');
-// const name = document.getElementById('name');
-// const username = document.getElementById('username');
-// const email = document.getElementById('email');
-// const password = document.getElementById('password');
-
-// document.getElementById('nomeEmpresa').textContent = userPerfil.novoNomeEmpresa;
-// document.getElementById('cnpj').textContent = userPerfil.novoCNPJ;
-// document.getElementById('adminPassword').textContent = userPerfil.senhaadmim;
-// document.getElementById('name').textContent = userPerfil.novoNomeUsuario;
-// document.getElementById('username').textContent = userPerfil.novoUsuario;
-// document.getElementById('email').textContent = userPerfil.novoEmail;
-// document.getElementById('password').textContent = userPerfil.alterarSenha;
-
 let listaUser = JSON.parse(localStorage.getItem('listaUser') || '[]');
 
-name.textContent = listaUser[0].nomeCad
-username.textContent = listaUser[0].usuarioCad
-password.textContent = listaUser[0].senhaCad
+function myFunction() {
 
-// // // if (userPerfil) {
-// // //     name.textContent = userPerfil.nomeCad;
-// // //     username.textContent = userPerfil.usuarioCad;
-// // //     password.textContent = userPerfil.senhaCad;
-// // // } else if (listaUser.length > 0) {
-// // //     name.textContent = listaUser[0].nomeCad;
-// // //     username.textContent = listaUser[0].usuarioCad;
-// // //     password.textContent = listaUser[0].senhaCad;
-// // }
+    document.getElementById('nomeEmpresa').textContent = userPerfil.novoNomeEmpresa;
+    document.getElementById('cnpj').textContent = userPerfil.novoCNPJ;
+    document.getElementById('adminPassword').textContent = userPerfil.senhaadmim;
+    // document.getElementById('name').textContent = userPerfil.novoNomeUsuario;
+    document.getElementById('username').textContent = userPerfil.novoUsuario;
+    document.getElementById('email').textContent = userPerfil.novoEmail;
+    document.getElementById('password').textContent = userPerfil.alterarSenha;
+    const name = document.getElementById('name');
 
+    if (userPerfil.novoNomeUsuario && userPerfil.novoNomeUsuario != "") {
+        name.textContent = userPerfil.novoNomeUsuario
+    } else if (!userPerfil.novoNomeUsuario || userPerfil.novoNomeUsuario == "") {
+        name.textContent = listaUser[0].nomeCad
+    }
+
+    if (userPerfil.novoUsuario && userPerfil.novoUsuario != "") {
+        username.textContent = userPerfil.novoUsuario
+    } else if (!userPerfil.novoUsuario || userPerfil.novoUsuario == "") {
+        username.textContent = listaUser[0].usuarioCad
+    }
+
+    if (userPerfil.alterarSenha && userPerfil.alterarSenha != "") {
+        password.textContent = userPerfil.alterarSenha
+    } else if (!userPerfil.alterarSenha || userPerfil.alterarSenha == "") {
+        password.textContent = listaUser[0].senhaCad
+    }
+
+}
 
 // // // ---------------------------------------------------
 
@@ -40,27 +39,39 @@ function mostrarSenha() {
     var inputPass = document.getElementById('password');
     var btnShowPass = document.getElementById('btn-senha');
 
+
     if (inputPass.innerHTML === '*********') {
         inputPass.innerHTML = listaUser[0].senhaCad;
         btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
+        if (userPerfil.alterarSenha && userPerfil.alterarSenha != "") {
+            inputPass.innerHTML = userPerfil.alterarSenha
+        } else if (!userPerfil.alterarSenha || userPerfil.alterarSenha == "") {
+            inputPass.innerHTML = listaUser[0].senhaCad
+        }
     } else {
         inputPass.innerHTML = '*********';
         btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
     }
 }
+
+
+
 
 function mostrarSenhaAdm() {
     var inputPass = document.getElementById('adminPassword');
     var btnShowPass = document.getElementById('btn-senhaadm');
 
-    if (inputPass.innerHTML === '*********') {
-        inputPass.innerHTML = userPerfil.adminPassword;
+    if (inputPass.innerHTML != userPerfil.senhaadmim) {
+        inputPass.innerHTML = userPerfil.senhaadmim;
         btnShowPass.classList.replace('bi-eye', 'bi-eye-slash');
-    } else {
-        inputPass.innerHTML = '*********';
+        if (userPerfil.senhaadmim && userPerfil.senhaadmim != "") {
+            inputPass.innerHTML = userPerfil.senhaadmim
+        } else if (!userPerfil.senhaadmim || userPerfil.senhaadmim == "") {
+            inputPass.innerHTML = userPerfil.senhaadmim
+        }
+    } else if (inputPass.innerHTML == userPerfil.senhaadmim) {
+        inputPass.innerHTML = '******';
         btnShowPass.classList.replace('bi-eye-slash', 'bi-eye');
     }
 }
-
-// -----------------------------
 
