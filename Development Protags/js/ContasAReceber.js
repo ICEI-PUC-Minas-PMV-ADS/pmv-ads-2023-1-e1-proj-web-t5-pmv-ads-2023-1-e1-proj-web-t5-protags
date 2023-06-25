363101f6e77e5b4b05ca66615540d28f1c3f65b3
Talysson Moraes Lourenço de Oliveira <talysson.lourenco@sga.pucminas.br>
2023-06-25 00:19:05 -0300

    Alterações Talysson

--- Development Protags/js/ContasAReceber.js
+++ Development Protags/js/ContasAReceber.js
@@ -1,173 +1,148 @@
 // Não permite que o usuário entre no sistema sem estar logado
 if (localStorage.getItem('token') === null) {
 
-  alert('Você precisa estar logado para acessar essa página')
-  window.location.href = 'login.html'
+  alert('Você precisa estar logado para acessar essa página');
+  window.location.href = 'login.html';
 
 } else {
 
   // Variáveis armazenadas para exibição
-  let conta = document.querySelector('#conta')
-  let vencimento = document.querySelector('#vencimento')
-  let parcela = document.querySelector('#parcela')
+  let conta = document.querySelector('#conta');
+  let vencimento = document.querySelector('#vencimento');
+  let parcela = document.querySelector('#parcela');
   let receberde = document.querySelector('#receberde')
-  let descricao = document.querySelector('#descricao')
-  let comprovante = document.querySelector('#comprovante')
+  let descricao = document.querySelector('#descricao');
+  let comprovante = document.querySelector('#comprovante');
   let dataDeRecebimento = document.querySelector('#dataDeRecebimento')
-  let exibirReais = document.querySelector('#exibirReais')
-  let selectMenu = document.querySelector('#select-menu')
-    //Dados para o menu "Contas Urgentes"
-    const rcontasAtrasadas = [];
-    const rdataAtual = new Date();
-    const menuUrgentes = document.querySelector('#menuUrgentes');
+  let exibirReais = document.querySelector('#exibirReais');
+  let selectMenu = document.querySelector('#select-menu');
+  let btnUrgentes = document.querySelector('#btnUrgentes');
+
+  //Dados para o menu "Contas Urgentes"
+  const rcontasAtrasadas = [];
+  const rdataAtual = new Date();
+  const menuUrgentes = document.querySelector('#menuUrgentes');
 
   // Formata a data para exibição
   function formatarData(data) {
-
     const partes = data.split('-');
-
     const dataFormatada = partes[2] + '/' + partes[1] + '/' + partes[0];
-
     return dataFormatada;
-
   }
 
-
-
-
-  // Array Contas A Receber recuperado do localStorage
-
-  let contasAReceber = JSON.parse(localStorage.getItem('contasAReceber') || '[]');
-
-
-
+  // Array contasAReceber recuperado do localStorage
+  let contasAReceber = JSON.parse(localStorage.getItem('contasAReceber')) || [];
 
   // Atribuir IDs individuais para cada conta
 
   contasAReceber.forEach((conta, index) => {
-
     conta.id = index + 1; // IDs começando em 1
-
   });
 
-
-
-
   const cardRealizados = document.querySelector('#table-exibicao');
-
   cardRealizados.innerHTML = '';
 
-
-
-
   for (let i = 0; i < contasAReceber.length; i++) {
-
     const conta = contasAReceber[i];
-
     const dataFormatadaVenci = formatarData(conta.dataderecebimento);
-
+    const dataFormatadaEmissao = formatarData(conta.datadeemissao);
 
     // Formata a data
 
     const newRow = document.createElement('tr');
 
     newRow.innerHTML = `
-
     <td class="text-center" id="conta">${conta.id}</td>
-
     <td class="text-center" id="vencimento">${dataFormatadaVenci}</td>
-
     <td class="text-center" id="parcela">${conta.parcelas}</td>
-
     <td class="text-center" id="receberde">${conta.receberde}</td>
-
     <td class="text-center" id="descricao">${conta.descricao}</td>
-
-    <td class="text-center" id="comprovante"><input type="file"></td>
-
-    <td class="text-center" id="dataDeVencimento"><input type="date"></td>
-
+    <td class="text-center" id="dataDePagamento_${i}"><input type="date" onchange="salvarETransferir(${i})"></td>
     <td class="text-center" id="exibirReais">${conta.valor}</td>
-
     <td class="text-center">
-
-      <select name="acoes" class="selectAcoes" id="select-menu-${conta.situacao}">
-
+          <select name="acoes" class="selectAcoes" id="select-apagar_${i}" onchange="salvarETransferir(${i})">
         <option value="3" class="opt">A RECEBER</option>
-
         <option value="6" class="opt">RECEBIDO</option>
-
       </select>
-
+      <button onclick="editarConta(${i})"><img class="editarIcon" src="./images/editar.png"></button>
+      <button onclick="removerConta(${i})"><img class="deleteIcon" src="./images/delete.png"></button>
     </td>
 
   `;
-
-
-
-
     cardRealizados.appendChild(newRow);
-
   }
 
-  btnUrgentes.addEventListener('click', () => {
-    if (menuUrgentes.style.display !== "block") {
-        menuUrgentes.style.display = "block";
-    } else {
-        menuUrgentes.style.display = "none";
-    }
-});
+  function editarConta(i) {
+    const conta = contasAReceber[i];
+    const queryString = `?conta=${conta.id}&datadeemissao=${conta.datadeemissao}&vencimento=${conta.datadevenci}&parcelas=${conta.parcelas}&pagarpara=${conta.pagarpara}&descricao=${conta.descricao}&valor=${conta.valor}&categoria=${conta.categoria}&condicaopag=${conta.condicaopag}`;
+    window.location.href = `./apagar.html${queryString}`;
+  }
 
-contasAReceber.forEach(conta => {
-  const dataVencimento = new Date(conta.dataderecebimento);
+
   
-  const dataLimite = new Date(dataVencimento);
-  dataLimite.setDate(dataLimite.getDate() - 3);
+  function deletarConta(i) {
+    const deleteIcon = document.getElementById(`deleteIcon-${i}`);
+    deleteIcon.addEventListener('click', () => {
+      let confirmar = confirm('Tem certeza de que deseja excluir?');
 
-  if (rdataAtual >= dataLimite && rdataAtual < dataVencimento) {
-    rcontasAtrasadas.push(conta);
-  }
-});
-
-  for (let i = 0; i < rcontasAtrasadas.length; i++) {
-    const bordaAppend = document.createElement('div');
-    bordaAppend.classList.add('urgentesBorda');
-    bordaAppend.innerHTML = `<div></div>`;
-    menuUrgentes.appendChild(bordaAppend);
-
-    const títuloAppend = document.createElement('div');
-    títuloAppend.classList.add('urgentesTítulo');
-    títuloAppend.innerHTML = `<div>Conta ${rcontasAtrasadas[i].conta}</div>`;
-    bordaAppend.appendChild(títuloAppend);
-
-    const datadeRecebimentoAppend = document.createElement('div');
-    datadeRecebimentoAppend.classList.add('urgentesDataDeRecebimento');
-    datadeRecebimentoAppend.innerHTML = `<div>Data de Recebimento: ${rcontasAtrasadas[i].dataderecebimento}</div>`;
-    bordaAppend.appendChild(datadeRecebimentoAppend);
-
-    const valorAppend = document.createElement('div');
-    valorAppend.classList.add('urgentesValor');
-    valorAppend.innerHTML = `<div>Valor: ${rcontasAtrasadas[i].valor}</div>`;
-    bordaAppend.appendChild(valorAppend);
-
-    const parcelasAppend = document.createElement('div');
-    parcelasAppend.classList.add('urgentesParcelas');
-    parcelasAppend.innerHTML = `<div>Parcelas: ${rcontasAtrasadas[i].parcelas}</div>`;
-    bordaAppend.appendChild(parcelasAppend);
-
-    const receberdeAppend = document.createElement('div');
-    receberdeAppend.classList.add('urgentesreceberde');
-    receberdeAppend.innerHTML = `<div>Receber De: ${rcontasAtrasadas[i].receberde}</div>`;
-    bordaAppend.appendChild(receberdeAppend);
+      if (confirmar === true) {
+        alert('Excluído com sucesso');
+        contasAReceber.splice(i, 1);
+        localStorage.setItem('contasAReceber', JSON.stringify(contasAReceber));
+        const cardToRemove = deleteIcon.parentNode.parentNode;
+        cardToRemove.parentNode.removeChild(cardToRemove);
+      }
+    });
   }
 
-  console.log(rcontasAtrasadas);
-  localStorage.setItem("contasAtrasadas", JSON.stringify(rcontasAtrasadas));
+  // Adicione o seguinte trecho de código ao final para garantir que o evento seja registrado corretamente:
+  const deleteIcons = document.querySelectorAll('[id^="deleteIcon-"]');
+  deleteIcons.forEach((deleteIcon, i) => {
+    deleteIcon.addEventListener('click', () => deletarConta(i));
+  });
 
-}
+  function salvarETransferir(i) {
+    let selectElement = document.getElementById(`select-apagar_${i}`);
+    let selectValue = selectElement.value;
 
-// Ao clicar em 'Sair', apaga o token de acesso, exigindo um novo login
-function logout() {
-  localStorage.removeItem('token')
-  window.location.href = 'login.html'
+    if (selectValue === '6') {
+      // Verifica se o valor selecionado é "PAGO"
+      let dataDePagamentoElement = document.getElementById(`dataDePagamento_${i}`);
+      let inputDataDePagamento = dataDePagamentoElement.querySelector('input');
+      let dataDePagamentoValue = inputDataDePagamento.value;
+
+      if (dataDePagamentoValue === '' || dataDePagamentoValue === null) {
+        inputDataDePagamento.setAttribute('style', 'border: solid 2px red');
+      } else {
+        inputDataDePagamento.removeAttribute('style');
+
+        let contaPagamento = { ...contasAReceber[i] };
+        contaPagamento.dataDePagamento = dataDePagamentoValue;
+
+        const contasPagas = JSON.parse(localStorage.getItem('contasPagas')) || [];
+        contasPagas.push(contaPagamento);
+        contasAReceber.splice(i, 1);
+
+        localStorage.setItem('contasAReceber', JSON.stringify(contasAReceber));
+        localStorage.setItem('contasPagas', JSON.stringify(contasPagas));
+
+        window.location.href = 'ContasPagas.html';
+      }
+    }
+  }
+
+  //eventListener para abrir e fechar o menu de contas urgentes
+  btnUrgentes.addEventListener('click', () => {
+    if (menuUrgentes.style.display !== 'block') {
+      menuUrgentes.style.display = 'block';
+    } else {
+      menuUrgentes.style.display = 'none';
+    }
+  });
+
+  // Ao clicar em 'Sair', apaga o token de acesso, exigindo um novo login
+  function logout() {
+    localStorage.removeItem('token');
+    window.location.href = 'login.html';
+  }
 }
--- Development Protags/js/ContasApagar.js
+++ Development Protags/js/ContasApagar.js
@@ -1,152 +1,144 @@
 // Não permite que o usuário entre no sistema sem estar logado
 if (localStorage.getItem('token') === null) {
-  alert('Você precisa estar logado para acessar essa página');
-  window.location.href = 'login.html';
-} else {
-  // Variáveis armazenadas para exibição
-  let conta = document.querySelector('#conta');
-  let vencimento = document.querySelector('#vencimento');
-  let parcela = document.querySelector('#parcela');
-  let pagarpara = document.querySelector('#pagarpara');
-  let descricao = document.querySelector('#descricao');
-  let comprovante = document.querySelector('#comprovante');
-  let dataDePagamento = document.querySelector('#dataDePagamento');
-  let exibirReais = document.querySelector('#exibirReais');
-  let selectMenu = document.querySelector('#select-menu');
-  let btnUrgentes = document.querySelector('#btnUrgentes');
-
-  //Dados para o menu "Contas Urgentes"
-  const pdataAtual = new Date();
-  const menuUrgentes = document.querySelector('#menuUrgentes');
-
-  // Formata a data para exibição
-  function formatarData(data) {
-    const partes = data.split('-');
-    const dataFormatada = partes[2] + '/' + partes[1] + '/' + partes[0];
-    return dataFormatada;
-  }
-
-  // Array contasAPagar recuperado do localStorage
-  let contasAPagar = JSON.parse(localStorage.getItem('contasAPagar')) || [];
-
-  // Atribuir IDs individuais para cada conta
-  contasAPagar.forEach((conta, index) => {
-    conta.id = index + 1; // IDs começando em 1
-  });
-
-  const cardRealizados = document.querySelector('#table-exibicao');
-
-  cardRealizados.innerHTML = '';
-
-  for (let i = 0; i < contasAPagar.length; i++) {
-    const conta = contasAPagar[i];
-
-    const dataFormatadaVenci = formatarData(conta.datadevenci);
-    const dataFormatadaEmissao = formatarData(conta.datadeemissao);
-
-    // Formata a data
-    const newRow = document.createElement('tr');
-
-    newRow.innerHTML = `
-    <td class="text-center" id="conta_${i}">${conta.id}</td>
-    <td class="text-center" id="parcela_${i}">${conta.parcelas}</td>
-    <td class="text-center" id="pagarpara_${i}">${conta.pagarpara}</td>
-    <td class="text-center" id="descricao_${i}">${conta.descricao}</td>
-    <td class="text-center" id="vencimento_${i}">${dataFormatadaVenci}</td>
-    <td class="text-center" id="dataDePagamento_${i}"><input type="date" onchange="salvarETransferir(${i})"></td>
-    <td class="text-center" id="exibirReais_${i}">${conta.valor}</td>
-    <td class="text-center">
-        <select name="acoes" class="selectAcoes" id="select-apagar_${i}" onchange="salvarETransferir(${i})">
-            <option value="3" class="opt">A PAGAR</option>
-            <option value="6" class="opt">PAGO</option>
-        </select>
-        <button onclick="editarConta(${i})"><img class="editarIcon" src="./images/editar.png"></button>
-        <button type="button" id="deleteIcon-${i}"><img src="./images/delete.png" class="deleteIcon"></button>
-    </td>
-    `;
-
-    cardRealizados.appendChild(newRow);
-  }
-
-  function editarConta(i) {
-    // Recupera os valores da conta com base na posição 'i' no array contasAPagar
-    const conta = contasAPagar[i];
+    alert('Você precisa estar logado para acessar essa página');
+    window.location.href = 'login.html';
+  } else {
+    // Variáveis armazenadas para exibição
+    let conta = document.querySelector('#conta');
+    let vencimento = document.querySelector('#vencimento');
+    let parcela = document.querySelector('#parcela');
+    let pagarpara = document.querySelector('#pagarpara');
+    let descricao = document.querySelector('#descricao');
+    let comprovante = document.querySelector('#comprovante');
+    let dataDePagamento = document.querySelector('#dataDePagamento');
+    let exibirReais = document.querySelector('#exibirReais');
+    let selectMenu = document.querySelector('#select-menu');
+    let btnUrgentes = document.querySelector('#btnUrgentes');
   
-    // Preenche os campos de edição com os valores da conta
-    contaInput.value = conta.conta;
-    vencimentoInput.value = conta.vencimento;
-    parcelaInput.value = conta.parcela;
-    pagarParaInput.value = conta.pagarpara;
-    descricaoInput.value = conta.descricao;
-    comprovanteInput.value = conta.comprovante;
+    //Dados para o menu "Contas Urgentes"
+    const pdataAtual = new Date();
+    const menuUrgentes = document.querySelector('#menuUrgentes');
   
-    // Abre o formulário de edição
-    abrirFormularioEdicao();
-  }
+    // Formata a data para exibição
+    function formatarData(data) {
+      const partes = data.split('-');
+      const dataFormatada = partes[2] + '/' + partes[1] + '/' + partes[0];
+      return dataFormatada;
+    }
   
-  function deletarConta(i) {
-    const deleteIcon = document.getElementById(`deleteIcon-${i}`);
-    deleteIcon.addEventListener('click', () => {
-      let confirmar = confirm('Tem certeza de que deseja excluir?');
-
-      if (confirmar === true) {
-        alert('Excluído com sucesso');
-        contasAPagar.splice(i, 1);
-        localStorage.setItem('contasAPagar', JSON.stringify(contasAPagar));
-        const cardToRemove = deleteIcon.parentNode.parentNode;
-        cardToRemove.parentNode.removeChild(cardToRemove);
+    // Array contasAPagar recuperado do localStorage
+    let contasAPagar = JSON.parse(localStorage.getItem('contasAPagar')) || [];
+  
+    // Atribuir IDs individuais para cada conta
+    contasAPagar.forEach((conta, index) => {
+      conta.id = index + 1; // IDs começando em 1
+    });
+  
+    const cardRealizados = document.querySelector('#table-exibicao');
+  
+    cardRealizados.innerHTML = '';
+  
+    for (let i = 0; i < contasAPagar.length; i++) {
+      const conta = contasAPagar[i];
+  
+      const dataFormatadaVenci = formatarData(conta.datadevenci);
+      const dataFormatadaEmissao = formatarData(conta.datadeemissao);
+  
+      // Formata a data
+      const newRow = document.createElement('tr');
+  
+      newRow.innerHTML = `
+      <td class="text-center" id="conta_${i}">${conta.id}</td>
+      <td class="text-center" id="parcela_${i}">${conta.parcelas}</td>
+      <td class="text-center" id="pagarpara_${i}">${conta.pagarpara}</td>
+      <td class="text-center" id="descricao_${i}">${conta.descricao}</td>
+      <td class="text-center" id="vencimento_${i}">${dataFormatadaVenci}</td>
+      <td class="text-center" id="dataDePagamento_${i}"><input type="date" onchange="salvarETransferir(${i})"></td>
+      <td class="text-center" id="exibirReais_${i}">${conta.valor}</td>
+      <td class="text-center">
+          <select name="acoes" class="selectAcoes" id="select-apagar_${i}" onchange="salvarETransferir(${i})">
+              <option value="3" class="opt">A PAGAR</option>
+              <option value="6" class="opt">PAGO</option>
+          </select>
+          <button onclick="editarConta(${i})"><img class="editarIcon" src="./images/editar.png"></button>
+          <button type="button" id="deleteIcon-${i}"><img src="./images/delete.png" class="deleteIcon"></button>
+      </td>
+      `;
+  
+      cardRealizados.appendChild(newRow);
+    }
+  
+    function editarConta(i) {
+      const conta = contasAPagar[i];
+      const queryString = `?conta=${conta.id}&datadeemissao=${conta.datadeemissao}&vencimento=${conta.datadevenci}&parcelas=${conta.parcelas}&pagarpara=${conta.pagarpara}&descricao=${conta.descricao}&valor=${conta.valor}&categoria=${conta.categoria}&condicaopag=${conta.condicaopag}`;
+      window.location.href = `./apagar.html${queryString}`;
+    }
+  
+  
+    
+    function deletarConta(i) {
+      const deleteIcon = document.getElementById(`deleteIcon-${i}`);
+      deleteIcon.addEventListener('click', () => {
+        let confirmar = confirm('Tem certeza de que deseja excluir?');
+  
+        if (confirmar === true) {
+          alert('Excluído com sucesso');
+          contasAPagar.splice(i, 1);
+          localStorage.setItem('contasAPagar', JSON.stringify(contasAPagar));
+          const cardToRemove = deleteIcon.parentNode.parentNode;
+          cardToRemove.parentNode.removeChild(cardToRemove);
+        }
+      });
+    }
+  
+    // Adicione o seguinte trecho de código ao final para garantir que o evento seja registrado corretamente:
+    const deleteIcons = document.querySelectorAll('[id^="deleteIcon-"]');
+    deleteIcons.forEach((deleteIcon, i) => {
+      deleteIcon.addEventListener('click', () => deletarConta(i));
+    });
+  
+    function salvarETransferir(i) {
+      let selectElement = document.getElementById(`select-apagar_${i}`);
+      let selectValue = selectElement.value;
+  
+      if (selectValue === '6') {
+        // Verifica se o valor selecionado é "PAGO"
+        let dataDePagamentoElement = document.getElementById(`dataDePagamento_${i}`);
+        let inputDataDePagamento = dataDePagamentoElement.querySelector('input');
+        let dataDePagamentoValue = inputDataDePagamento.value;
+  
+        if (dataDePagamentoValue === '' || dataDePagamentoValue === null) {
+          inputDataDePagamento.setAttribute('style', 'border: solid 2px red');
+        } else {
+          inputDataDePagamento.removeAttribute('style');
+  
+          let contaPagamento = { ...contasAPagar[i] };
+          contaPagamento.dataDePagamento = dataDePagamentoValue;
+  
+          const contasPagas = JSON.parse(localStorage.getItem('contasPagas')) || [];
+          contasPagas.push(contaPagamento);
+          contasAPagar.splice(i, 1);
+  
+          localStorage.setItem('contasAPagar', JSON.stringify(contasAPagar));
+          localStorage.setItem('contasPagas', JSON.stringify(contasPagas));
+  
+          window.location.href = 'ContasPagas.html';
+        }
+      }
+    }
+  
+    //eventListener para abrir e fechar o menu de contas urgentes
+    btnUrgentes.addEventListener('click', () => {
+      if (menuUrgentes.style.display !== 'block') {
+        menuUrgentes.style.display = 'block';
+      } else {
+        menuUrgentes.style.display = 'none';
       }
     });
-  }
-
-  // Adicione o seguinte trecho de código ao final para garantir que o evento seja registrado corretamente:
-  const deleteIcons = document.querySelectorAll('[id^="deleteIcon-"]');
-  deleteIcons.forEach((deleteIcon, i) => {
-    deleteIcon.addEventListener('click', () => deletarConta(i));
-  });
-
-  function salvarETransferir(i) {
-    let selectElement = document.getElementById(`select-apagar_${i}`);
-    let selectValue = selectElement.value;
-
-    if (selectValue === '6') {
-      // Verifica se o valor selecionado é "PAGO"
-      let dataDePagamentoElement = document.getElementById(`dataDePagamento_${i}`);
-      let inputDataDePagamento = dataDePagamentoElement.querySelector('input');
-      let dataDePagamentoValue = inputDataDePagamento.value;
-
-      if (dataDePagamentoValue === '' || dataDePagamentoValue === null) {
-        inputDataDePagamento.setAttribute('style', 'border: solid 2px red');
-      } else {
-        inputDataDePagamento.removeAttribute('style');
-
-        let contaPagamento = { ...contasAPagar[i] };
-        contaPagamento.dataDePagamento = dataDePagamentoValue;
-
-        const contasPagas = JSON.parse(localStorage.getItem('contasPagas')) || [];
-        contasPagas.push(contaPagamento);
-        contasAPagar.splice(i, 1);
-
-        localStorage.setItem('contasAPagar', JSON.stringify(contasAPagar));
-        localStorage.setItem('contasPagas', JSON.stringify(contasPagas));
-
-        window.location.href = 'ContasPagas.html';
-      }
+  
+    // Ao clicar em 'Sair', apaga o token de acesso, exigindo um novo login
+    function logout() {
+      localStorage.removeItem('token');
+      window.location.href = 'login.html';
     }
   }
-
-  //eventListener para abrir e fechar o menu de contas urgentes
-  btnUrgentes.addEventListener('click', () => {
-    if (menuUrgentes.style.display !== 'block') {
-      menuUrgentes.style.display = 'block';
-    } else {
-      menuUrgentes.style.display = 'none';
-    }
-  });
-
-  // Ao clicar em 'Sair', apaga o token de acesso, exigindo um novo login
-  function logout() {
-    localStorage.removeItem('token');
-    window.location.href = 'login.html';
-  }
-}
